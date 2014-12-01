a61facc4e1cedcde87bc795a7a60abae2491acb7
Jay Ruffing <jay.ruffing@blueshieldca.com>
2014-12-01 00:01:34 -0600

    Update appium.rb

--- Casks/appium.rb
+++ Casks/appium.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'appium' do
-  version '1.3.1'
-  sha256 '5c45a6fac32e9db5d23fb0ce74dd9ce80f1b187683e889f57d5dda6a66ef281d'
+  version '1.3.3'
+  sha256 '11249e13ca0c3ad17dd4754706c135e71d33c62c7b546f359ff7a01821d29542'
 
   url "https://bitbucket.org/appium/appium.app/downloads/appium-#{version}.dmg"
   homepage 'http://appium.io'
